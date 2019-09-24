4a830b8be7aad4f1fce986c1d2eeb7fc524fcd02
hp-liangxw <xuewei.liang@hongpucorp.com>
2019-09-24 17:03:36 +0800

    新增wheels以及battery项目

--- el_detectron/a.py
+++
@@ -1,9 +0,0 @@
-
-
-answer = input("Is the configuration file correct? [yes/no]:")
-while answer.upper() not in ["YES", "Y", "NO", "N"]:
-    print("Please input yes or no!")
-    answer = input()
-
-if answer.upper() in ["YES", "Y"]:
-    print("nice")
